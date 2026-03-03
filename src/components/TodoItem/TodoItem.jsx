76e8dbd182e6e9e92986b617a864082a0f0926ce
Badri Satwik <Badrisatwik007@gmail.com>
2026-03-03 16:03:07 +0530

    feat: implement delete task functionality

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,18 +3,23 @@ import { todosContext } from "../../context/context"
 // const {Todos,setTodos} = useContext(todosContext)
 
 const TodoItem = () => {
-    const {Todos,setTodos} = useContext(todosContext)
-  return (
-    <div>
-     {Todos.map((item) => {
-        return <p key={item.id}>
-            {item.Todo} 
-            <button>Edit</button>
-            <button>Delete</button>
-        </p>
-     })}
-    </div>
-  )
+    const { Todos, setTodos } = useContext(todosContext)
+
+    function handleDelete(id) {
+        const newTodos = Todos.filter((todo) => todo.id !== id)
+        setTodos(newTodos)
+    }
+    return (
+        <div>
+            {Todos.map((item) => {
+                return <p key={item.id}>
+                    {item.Todo}
+                    <button>Edit</button>
+                    <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </p>
+            })}
+        </div>
+    )
 }
 
 export default TodoItem
