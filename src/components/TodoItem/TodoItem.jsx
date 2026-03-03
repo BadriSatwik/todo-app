5ba396a38bb588f62bbcacb37efcc606cb15d17b
Badri Satwik <Badrisatwik007@gmail.com>
2026-03-03 16:32:52 +0530

    feat: add task completion toggle feature

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react"
 import { todosContext } from "../../context/context"
 // const {Todos,setTodos} = useContext(todosContext)
 
+import './TodoItem.css'
+
 const TodoItem = () => {
     const { Todos, setTodos } = useContext(todosContext)
 
@@ -9,14 +11,22 @@ const TodoItem = () => {
         const newTodos = Todos.filter((todo) => todo.id !== id)
         setTodos(newTodos)
     }
+
+    function handleCheckbox(id) {
+        const newTodos = Todos.map((todo) =>
+            todo.id === id ? { ...todo, isFinished: !todo.isFinished } : todo
+        )
+        setTodos(newTodos)
+    }
     return (
         <div>
             {Todos.map((item) => {
-                return <p key={item.id}>
-                    {item.Todo}
+                return <div key={item.id}>
+                    <input type="checkbox" checked={item.isFinished} onChange={() => handleCheckbox(item.id)} />
+                    <div className={item.isFinished ? "line-through" : ""}>{item.Todo}</div>
                     <button>Edit</button>
                     <button onClick={() => handleDelete(item.id)}>Delete</button>
-                </p>
+                </div>
             })}
         </div>
     )
